f6078b9a21bde473cbaa5f2bf19bf48de54a5477
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-20 15:52:12 +0530

    Initial Commit

---
+++ Menu_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Menu_App
+//
+//  Created by Prajwal U on 20/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Menu_App/Menu_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_AppApp.swift
+//  Menu_App
+//
+//  Created by Prajwal U on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Menu_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
