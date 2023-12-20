ac7e22ba6aeb60e16412ab624bc57073e091127d
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2023-12-20 19:01:05 +0530

    Mod menu app

---
+++ Menu_App/DataService.swift
@@ -0,0 +1,8 @@
+//
+//  DataService.swift
+//  Menu_App
+//
+//  Created by Prajwal U on 20/12/23.
+//
+
+import Foundation
---
+++ Menu_App/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  Menu_App
+//
+//  Created by Prajwal U on 20/12/23.
+//
+
+import Foundation
